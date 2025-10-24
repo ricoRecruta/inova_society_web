a1e59c96aa594486b4647850efc121835f839218
João Miguel Santos <158611854+ricoRecruta@users.noreply.github.com>
2025-10-24 01:03:13 -0300

    Resolvendo o importe errado do contact.tsx

--- INOVA/src/components/Contact.tsx
+++ INOVA/src/components/Contact.tsx
@@ -4,7 +4,7 @@ import { Input } from './ui/input';
 import { Textarea } from './ui/textarea';
 import { Label } from './ui/label';
 import { Mail, MapPin, Phone, Send, Linkedin, Github, Instagram } from 'lucide-react';
-import { toast } from 'sonner@2.0.3';
+import { toast } from 'sonner';
 export function Contact() {
   const [formData, setFormData] = useState({
     name: '',
